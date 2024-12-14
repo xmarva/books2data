55e2a0d7ec8f9b779d18c9618dbe10bdc310d329
xmarva <elizaveta.victoria.koroleva@gmail.com>
2024-12-14 17:17:31 -0300

    deleting removed files

--- core/file_detector.py
+++
@@ -1,8 +0,0 @@
-# text_processing_lib/core/file_detector.py
-import os
-
-class FileDetector:
-
-    def detect_file_type(self, file_path):
-        _, ext = os.path.splitext(file_path)
-        return ext.lower()
